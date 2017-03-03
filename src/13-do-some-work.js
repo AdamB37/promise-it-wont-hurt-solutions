9dac9176d03697151de1d0e6c5ef2d7d2adb4bfa
AdamB37 <adam.beshir@gmail.com>
2017-03-03 13:09:09 -0800

    completed

--- src/13-do-some-work.js
+++ src/13-do-some-work.js
@@ -1,12 +1,9 @@
 var HTTP = require("q-io/http")
 
 HTTP.read('http://localhost:7000')
-.then((response) => {
-  console.log(JSON.parse(response))
-})
+.then((response) => response.toString())
 .then(response => {
-  console.log(response);
-  HTTP.read(`http://localhost:7001/${1}`,response)
+  HTTP.read(`http://localhost:7001/${response}`)
   .then(JSON.parse)
   .then(console.log)
 })
